8f871e2bda3c3b96bf16c0ed874772d94de9ea10
Cody Knapp <cody.knapp@accenture.com>
2020-05-08 13:19:16 -0400

    Adding pencilboard test to verify click handler behavior

--- src/components/PencilBoard.test.js
+++ src/components/PencilBoard.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 import PencilBoard from './PencilBoard';
+import PencilCell from './PencilCell';
 
 describe ('Basic Functionality', () => {
     it('renders without crashing', () => {
@@ -12,6 +13,14 @@ describe ('Basic Functionality', () => {
         const onClick = sinon.spy();
         const pencilBoard = shallow(<PencilBoard/>);
         pencilBoard.find('.pencilCell').first().simulate('click');
+        expect(pencilBoard.state().value).toBe(1);
+        expect(pencilBoard.find('.pencilCell')).toHaveLength(1);
+    })
+
+    it('binds a state changing function the PencilCell', () => {
+        const pencilBoard = shallow(<PencilBoard/>);
+        const clickHandler = pencilBoard.find(PencilCell).first().prop('handleClick');
+        clickHandler();
         expect(pencilBoard.state().value).toBe(1);
         expect(pencilBoard.find('.pencilCell')).toHaveLength(1);
     })
