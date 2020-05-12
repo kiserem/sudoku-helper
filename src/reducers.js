c99ce3225e3fdd8a53434cec794cc7c5aae68491
Erin Kiser <ekiser4@ford.com>
2020-05-11 21:01:51 -0400

    Reducer now removes inelligible numbers from the proper row and column

--- src/reducers.js
+++ src/reducers.js
@@ -7,53 +7,127 @@ function selectValueReducer(state={}, action){
             let cell_value = action.cell_selected.cell_value;
             let selected_cell = action.cell_selected.cell - 1;
             let selected_block = action.cell_selected.sudoku_block - 1;
+            let selected_row = action.cell_selected.sudoku_row;
+            let selected_column = action.cell_selected.sudoku_column;
 
             newState.sudoku_board.game.boards[action.cell_selected.sudoku_block - 1].cells[selected_cell].pencil_board.cell_value = cell_value;
 
             switch(cell_value) {
                 case 1:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_1 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_1 = null;
+                            }
+                        }
                     }
                     break;
                 case 2:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_2 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_2 = null;
+                            }
+                        }
                     }
                     break;
                 case 3:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_3 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_3 = null;
+                            }
+                        }
                     }
                     break;
                 case 4:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_4 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_4 = null;
+                            }
+                        }
                     }
                     break;
                 case 5:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_5 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_5 = null;
+                            }
+                        }
                     }
                     break;
                 case 6:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_6 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_6 = null;
+                            }
+                        }
                     }
                     break;
                 case 7:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_7 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_7 = null;
+                            }
+                        }
                     }
                     break;
                 case 8:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_8 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_8 = null;
+                            }
+                        }
                     }
                     break;
                 case 9:
-                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
-                        val.pencil_board.pencil_9 = null;
+                    for(let [index, block] of newState.sudoku_board.game.boards.entries()) {
+                        for(let [index, cell] of block.cells.entries()) {
+                            if (
+                                cell.sudoku_block - 1 === selected_block ||
+                                cell.sudoku_row === selected_row ||
+                                cell.sudoku_column === selected_column
+                            ) {
+                                cell.pencil_board.pencil_9 = null;
+                            }
+                        }
                     }
                     break;
             }
