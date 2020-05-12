5325be943be1fb1b3f85587e5f9f835dd8fb2183
Erin Kiser <ekiser4@ford.com>
2020-05-11 20:48:13 -0400

    Make the reducer a little cleaner

--- src/reducers.js
+++ src/reducers.js
@@ -12,103 +12,49 @@ function selectValueReducer(state={}, action){
 
             switch(cell_value) {
                 case 1:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_1 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_1 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_1 = null;
+                    }
                     break;
                 case 2:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_2 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_2 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_2 = null;
+                    }
                     break;
                 case 3:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_3 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_3 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_3 = null;
+                    }
                     break;
                 case 4:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_4 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_4 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_4 = null;
+                    }
                     break;
                 case 5:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_5 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_5 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_5 = null;
+                    }
                     break;
                 case 6:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_6 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_6 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_6 = null;
+                    }
                     break;
                 case 7:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_7 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_7 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_7 = null;
+                    }
                     break;
                 case 8:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_8 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_8 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_8 = null;
+                    }
                     break;
                 case 9:
-                    newState.sudoku_board.game.boards[selected_block].cells[0].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[1].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[2].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[3].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[4].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[5].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[6].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[7].pencil_board.pencil_9 = null;
-                    newState.sudoku_board.game.boards[selected_block].cells[8].pencil_board.pencil_9 = null;
+                    for(let [index, val] of newState.sudoku_board.game.boards[selected_block].cells.entries()) {
+                        val.pencil_board.pencil_9 = null;
+                    }
                     break;
             }
             return newState;
