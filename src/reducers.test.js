d45dd1dfb7c97a0b6f66e0faf48ab1da249a4229
Erin Kiser <ekiser4@ford.com>
2020-05-13 11:31:48 -0400

    Make sure we are returning a state that does not equal what we were given.

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -5,6 +5,21 @@ import { SELECT_VALUE } from './selectValueAction';
 describe('Reducers', () => {
     describe('SELECT_VALUE case', () => {
 
+        it('resulting state is not the starting state', () => {
+            expect(
+                reducer(initial_state, {
+                    type: SELECT_VALUE,
+                    cell_selected: {
+                        sudoku_block: 1,
+                        sudoku_row: 1,
+                        sudoku_column: 1,
+                        cell: 1,
+                        cell_value: 1,
+                    }
+                }).current
+            ).not.toEqual(initial_state);
+        });
+
         it('increments current', () => {
             expect(
                 reducer(initial_state, {
