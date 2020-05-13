4031b91f906340574c3df2225890e30883c57be8
Erin Kiser <ekiser4@ford.com>
2020-05-12 20:03:32 -0400

    Adding some imbedded sudoku boards from websudoku

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import Game from "./components/Game";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <Game/>
-      </header>
+        <div class="websudoku-wrapper">
+            <div className="websudoku">
+                <h1>Easy Sudoku</h1>
+                <iframe src="//widget.websudoku.com/?level=1" width="200" height="260" scrolling="no"
+                        frameBorder="0"></iframe>
+            </div>
+            <div className="websudoku">
+                <h1>Medium Sudoku</h1>
+                <iframe src="//widget.websudoku.com/?level=2" width="200" height="260" scrolling="no"
+                        frameBorder="0"></iframe>
+            </div>
+            <div className="websudoku">
+                <h1>Hard Sudoku</h1>
+                <iframe src="//widget.websudoku.com/?level=3" width="200" height="260" scrolling="no"
+                        frameBorder="0"></iframe>
+            </div>
+        </div>
+        <header className="App-header">
+            <Game/>
+        </header>
     </div>
   );
 }
